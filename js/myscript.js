6f8b035af88c48f5ddd071fd0a3262fbe8d93022
18suresha <asuresh180@gmail.com>
2015-06-22 19:41:48 -0400

    File Upload 3

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,19 @@
+// smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#content-carousel])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 // functions for content page text-appear links
 function link1Function() {
                 document.getElementById('link1-txt').style.display='block';
